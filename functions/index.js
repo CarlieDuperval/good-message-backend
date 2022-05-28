f174b7a0efa59ce11b0adfc65494cc114256c97d
Carlie Duperval <carlierichell@gmail.com>
2022-05-27 23:46:23 -0400

    Set routes, import express and cors on index.js

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,6 @@
 import functions from 'firebase-functions'
-import express from 'express'
-import cors from 'cors'
+import express from 'express';
+import cors from 'cors';
 
 
 
@@ -9,3 +9,9 @@ const app = express()
 app.use(cors());
 app.use(express.json());
 
+
+// set up routes
+
+app.get('/test', (req, res) => {req.send('Hi🔥🔥 This is working')})
+app.post('/goodMessages',            )
+app.get('/goodMessages',            )
